92df2ba0a726a5ec3db6dd57a63b4745118d8505
Soham2407 <nesarikarsoham24@gmail.com>
2023-02-12 19:17:16 +0530

    basic route added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
