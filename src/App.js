d3e593425a797567141e306c5d8451bf76de7e4e
Soham2407 <nesarikarsoham24@gmail.com>
2023-02-12 19:25:33 +0530

    Link component added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
-    <div>
+    <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
