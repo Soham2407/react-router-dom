887b69b6613592f7f6b5cdbcf966c2cd9eab9934
Soham2407 <nesarikarsoham24@gmail.com>
2023-02-12 20:23:00 +0530

    No match route page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import OrderSummery from "./components/OrderSummery";
+import NoMatch from "./components/NoMatch";
 import "./App.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="order-summary" element={<OrderSummery />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </>
   );
